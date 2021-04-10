a64a00aa368c6581ba6c9e74ef4c31abc6fc2a76
ashu741 <hars1hit10@gmail.com>
2021-04-10 13:14:25 +0530

    Create Mapper_Ashutosh.java

---
+++ Mapper_Ashutosh.java
@@ -0,0 +1,20 @@
+
+import java.io.IOException;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+
+public class Mapper_Ashutosh extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String inputstring = value.toString();
+		for (String x : inputstring.split(" ")) {
+			if (x.equals("Apple") || x.equals("Banana") || x.equals("Grapes")) {
+				context.write(new Text(x), new IntWritable(1));
+			}
+		}
+	}
+}
